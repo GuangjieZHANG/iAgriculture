14ecadf373f42b84579d25e443777c632e8b8554
guangjie zhang <guangjie.zhang@docdoku.com>
2020-02-12 17:40:51 +0100

    Change labels

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -18,7 +18,7 @@ function Copyright() {
         <Typography variant="body2" color="textSecondary" align="center">
             {'Copyright © '}
             <Link color="inherit" href="https://material-ui.com/">
-                西安环视
+                智慧农业
             </Link>{' '}
             {new Date().getFullYear()}
             {'.'}
